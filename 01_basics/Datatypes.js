a65e8aad645c327c07046c017fc90c05a7cfac48
NandaniKshirsagar <nandanikshirsagar8@gmail.com>
2025-12-15 12:57:09 +0000

    datatypes

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // it treats the code as newer Js version
+
+//Datatypes
+
+let name = "Nandani"; //String literal
+let Age = 19; //Number literal
+let isLoggedIn = true; //Boolean literal
+
+console.log(typeof isLoggedIn);
+console.log(typeof name);
+console.log(typeof Age);
+
+//primitive datatypes
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof null); //bug in js it shows object but it is primitive datatype
---
+++ 01_basics/Variables.js
@@ -0,0 +1,14 @@
+const accountId = "abc123";
+let accountEmail="Nandanikshirsagar64@gmail.com"
+var accountPassword = "password@123";
+let accountCity;
+accountEmail="sakshi@gmail.com"
+console.table([accountId, accountEmail, accountPassword,accountCity]);
+
+
+accountEmail="sakshi@gmail.com"
+console.log(accountEmail);
+/*
+not to use var because of issue in block space and functional space
+
+*/
